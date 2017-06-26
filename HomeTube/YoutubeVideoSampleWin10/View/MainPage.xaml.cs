887fd71d0ae1d37e99f8dd3bd39fdab94060671f
pham0uz <cpham@hotmail.de>
2017-06-26 16:58:33 +0200

    Update

--- HomeTube/YoutubeVideoSampleWin10/View/MainPage.xaml.cs
+++ HomeTube/YoutubeVideoSampleWin10/View/MainPage.xaml.cs
@@ -18,6 +18,7 @@ using Windows.UI.Xaml.Navigation;
 using Google.Apis.Services;
 using Google.Apis.YouTube.v3;
 using HomeTube.Model;
+using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -42,8 +43,6 @@ namespace HomeTube.View
                     //The number of videos you would like to get in one request(from 1 to 50)
                     int max_results = 50;
 
-
-                    
                     ////Channel Videos
                     ChannelVideos.Visibility = Visibility.Collapsed;
                     ChannelProgress.Visibility = Visibility.Visible;
@@ -74,7 +73,7 @@ namespace HomeTube.View
                     PlaylistVideos.Visibility = Visibility.Visible;
                     PlaylistProgress.Visibility = Visibility.Collapsed;
                     /////////
-                    
+
                 }
                 else
                 {
@@ -202,6 +201,144 @@ namespace HomeTube.View
             var channelIdResponse = await channelIdRequest.ExecuteAsync();
 
             return channelIdResponse.Items.FirstOrDefault().Id;
+        }
+
+
+        // Auto-Suggest Box
+        private ObservableCollection<String> suggestions;
+
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+
+        {
+
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+
+            {
+
+                //suggestions.Clear();
+
+                //suggestions.Add(sender.Text + "1");
+
+                //suggestions.Add(sender.Text + "2");
+
+                //suggestions.Add(sender.Text + "3");
+
+                //suggestions.Add(sender.Text + "4");
+
+                //suggestions.Add(sender.Text + "5");
+
+                //suggestions.Add(sender.Text + "6");
+
+                //suggestions.Add(sender.Text + "7");
+
+                //suggestions.Add(sender.Text + "8");
+
+                //suggestions.Add(sender.Text + "9");
+
+                //sender.ItemsSource = suggestions;
+
+            }
+
+        }
+
+        private async void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+
+        {
+
+            if (args.ChosenSuggestion != null)
+
+                txtAutoSuggestBox.Text = args.ChosenSuggestion.ToString();
+
+            else
+
+                txtAutoSuggestBox.Text = sender.Text;
+
+            SearchItems.Visibility = Visibility.Collapsed;
+            SearchProgress.Visibility = Visibility.Visible;
+
+            var searchItems = await ListItems(txtAutoSuggestBox.Text, 50);
+            SearchItems.ItemsSource = searchItems;
+
+            SearchItems.Visibility = Visibility.Visible;
+            SearchProgress.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+
+        {
+
+            txtAutoSuggestBox.Text = "Choosen";
+
+        }
+
+        // Search Run
+        private async Task SearchFor(string searchQuery)
+        {
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = searchQuery; // Replace with your search term.
+            searchListRequest.MaxResults = 50;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            List<string> videos = new List<string>();
+            List<string> channels = new List<string>();
+            List<string> playlists = new List<string>();
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+                        break;
+
+                    case "youtube#channel":
+                        channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
+                        break;
+
+                    case "youtube#playlist":
+                        playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
+                        break;
+                }
+            }
+
+            //Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
+            //Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
+            //Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
+        }
+
+        //Get Channel Videos
+        public async Task<List<YoutubeVideo>> ListItems(string searchQuery, int maxResults)
+        {
+            var searchItemsListRequest = youtubeService.Search.List("snippet");
+            searchItemsListRequest.Q = searchQuery;
+            //searchItemsListRequest.Type = "video";
+            searchItemsListRequest.Order = Google.Apis.YouTube.v3.SearchResource.ListRequest.OrderEnum.Date;
+            searchItemsListRequest.MaxResults = maxResults;
+            searchItemsListRequest.PageToken = "";
+
+            var searchItemsListResponse = await searchItemsListRequest.ExecuteAsync();
+            List<YoutubeVideo> searchItems = new List<YoutubeVideo>();
+
+            foreach (var searchItem in searchItemsListResponse.Items)
+            {
+                searchItems.Add(
+                    new YoutubeVideo
+                    {
+                        Id = searchItem.Id.VideoId,
+                        Title = searchItem.Snippet.Title,
+                        Description = searchItem.Snippet.Description,
+                        PubDate = searchItem.Snippet.PublishedAt,
+                        Thumbnail = searchItem.Snippet.Thumbnails.Medium.Url,
+                        YoutubeLink = "https://www.youtube.com/watch?v=" + searchItem.Id.VideoId
+                    });
+            }
+            return searchItems;
         }
 
 
