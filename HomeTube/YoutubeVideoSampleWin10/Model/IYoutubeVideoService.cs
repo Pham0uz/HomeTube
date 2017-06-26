9924f43719c16f439fc767db8bf3a037db691d99
karin <karin@DESKTOP-B3R37EK>
2017-06-26 19:22:29 +0200

    add commands, restruct , add interface and move methods to new class

---
+++ HomeTube/YoutubeVideoSampleWin10/Model/IYoutubeVideoService.cs
@@ -0,0 +1,27 @@
+﻿using HomeTube.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeVideoSpace
+{
+    public interface IYoutubeVideoService
+    {
+
+        //Get Channel Videos
+        Task<List<YoutubeVideo>> GetChannelVideos(string channelId, int maxResults);
+
+        Task<List<YoutubeVideo>> GetChannelVideosWithPagination(string channelId, int maxResults);
+
+        Task<List<YoutubeVideo>> GetPlaylistVideos(string playlistId, int maxResults);
+
+        Task<string> GetChannelId(string userName);
+
+        Task<List<YoutubeVideo>> ListItems(string searchQuery, int maxResults);
+
+
+
+    }
+}
--- HomeTube/YoutubeVideoSampleWin10/View/MainPage.xaml.cs
+++ HomeTube/YoutubeVideoSampleWin10/View/MainPage.xaml.cs
@@ -1,22 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net.NetworkInformation;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Google.Apis.Services;
-using Google.Apis.YouTube.v3;
 using HomeTube.Model;
 using System.Collections.ObjectModel;
 
@@ -29,9 +16,11 @@ namespace HomeTube.View
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private YoutubeVideoSpace.IYoutubeVideoService m_youtubeService;
         public MainPage()
         {
             this.InitializeComponent();
+            m_youtubeService = new YoutubeVideo
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
@@ -85,125 +74,7 @@ namespace HomeTube.View
 
             base.OnNavigatedTo(e);
         }
-
-
-        //Youtub Data API Credentials
-        YouTubeService youtubeService = new YouTubeService(
-                new BaseClientService.Initializer
-                {
-                    ApiKey = "AIzaSyDaeHjO9QK38vHvHHkvPVbrmx0iAi8M0cc",
-                    ApplicationName = "HomeTube"
-                });
-
-        //Get Channel Videos
-        public async Task<List<YoutubeVideo>> GetChannelVideos(string channelId, int maxResults)
-        {
-            var channelItemsListRequest = youtubeService.Search.List("snippet");
-            channelItemsListRequest.ChannelId = channelId;
-            channelItemsListRequest.Type = "video";
-            channelItemsListRequest.Order = Google.Apis.YouTube.v3.SearchResource.ListRequest.OrderEnum.Date;
-            channelItemsListRequest.MaxResults = maxResults;
-            channelItemsListRequest.PageToken = "";
-
-            var channelItemsListResponse = await channelItemsListRequest.ExecuteAsync();
-            List<YoutubeVideo> channelVideos = new List<YoutubeVideo>();
-
-            foreach (var channelItem in channelItemsListResponse.Items)
-            {
-                channelVideos.Add(
-                    new YoutubeVideo
-                    {
-                        Id = channelItem.Id.VideoId,
-                        Title = channelItem.Snippet.Title,
-                        Description = channelItem.Snippet.Description,
-                        PubDate = channelItem.Snippet.PublishedAt,
-                        Thumbnail = channelItem.Snippet.Thumbnails.Medium.Url,
-                        YoutubeLink = "https://www.youtube.com/watch?v=" + channelItem.Id.VideoId
-                    });
-            }
-
-            return channelVideos;
-        }
-
-        //Get Channel Videos With Pagination
-        public async Task<List<YoutubeVideo>> GetChannelVideosWithPagination(string channelId, int maxResults)
-        {
-            var channelItemsListRequest = youtubeService.Search.List("snippet");
-            channelItemsListRequest.ChannelId = channelId;
-            channelItemsListRequest.Type = "video";
-            channelItemsListRequest.Order = Google.Apis.YouTube.v3.SearchResource.ListRequest.OrderEnum.Date;
-            channelItemsListRequest.MaxResults = maxResults;
-            channelItemsListRequest.PageToken = "";
-
-            var channelItemsListResponse = await channelItemsListRequest.ExecuteAsync();
-            List<YoutubeVideo> channelVideos = new List<YoutubeVideo>();
-            var nextPageToken = "";
-            int i = 0;
-            while (i < 3)
-            {
-                foreach (var channelItem in channelItemsListResponse.Items)
-                {
-                    channelVideos.Add(
-                        new YoutubeVideo
-                        {
-                            Id = channelItem.Id.VideoId,
-                            Title = channelItem.Snippet.Title,
-                            Description = channelItem.Snippet.Description,
-                            PubDate = channelItem.Snippet.PublishedAt,
-                            Thumbnail = channelItem.Snippet.Thumbnails.Medium.Url,
-                            YoutubeLink = "https://www.youtube.com/watch?v=" + channelItem.Id.VideoId
-                        });
-                }
-
-                i++;
-
-                nextPageToken = channelItemsListResponse.NextPageToken;
-
-            }
-
-            return channelVideos;
-        }
-
-        //Get Playlist Videos
-        public async Task<List<YoutubeVideo>> GetPlaylistVideos(string playlistId, int maxResults)
-        {
-            var playlistItemsListRequest = youtubeService.PlaylistItems.List("snippet");
-            playlistItemsListRequest.PlaylistId = playlistId;
-            playlistItemsListRequest.MaxResults = maxResults;
-            playlistItemsListRequest.PageToken = "";
-
-            var playlistItemsListResponse = await playlistItemsListRequest.ExecuteAsync();
-            List<YoutubeVideo> playlistVideos = new List<YoutubeVideo>();
-
-            foreach (var playlistItem in playlistItemsListResponse.Items)
-            {
-                playlistVideos.Add(
-                    new YoutubeVideo
-                    {
-                        Id = playlistItem.Snippet.ResourceId.VideoId,
-                        Title = playlistItem.Snippet.Title,
-                        Description = playlistItem.Snippet.Description,
-                        PubDate = playlistItem.Snippet.PublishedAt,
-                        Thumbnail = playlistItem.Snippet.Thumbnails.Medium.Url,
-                        YoutubeLink = "https://www.youtube.com/watch?v=" + playlistItem.Snippet.ResourceId.VideoId
-                    });
-            }
-
-            return playlistVideos;
-        }
-
-        //Get Cannel Id
-        public async Task<string> GetChannelId(string userName)
-        {
-            var channelIdRequest = youtubeService.Channels.List("id");
-            channelIdRequest.ForUsername = userName;
-
-            var channelIdResponse = await channelIdRequest.ExecuteAsync();
-
-            return channelIdResponse.Items.FirstOrDefault().Id;
-        }
-
-
+         
         // Auto-Suggest Box
         private ObservableCollection<String> suggestions;
 
@@ -271,76 +142,6 @@ namespace HomeTube.View
             txtAutoSuggestBox.Text = "Choosen";
 
         }
-
-        // Search Run
-        private async Task SearchFor(string searchQuery)
-        {
-
-            var searchListRequest = youtubeService.Search.List("snippet");
-            searchListRequest.Q = searchQuery; // Replace with your search term.
-            searchListRequest.MaxResults = 50;
-
-            // Call the search.list method to retrieve results matching the specified query term.
-            var searchListResponse = await searchListRequest.ExecuteAsync();
-
-            List<string> videos = new List<string>();
-            List<string> channels = new List<string>();
-            List<string> playlists = new List<string>();
-
-            // Add each result to the appropriate list, and then display the lists of
-            // matching videos, channels, and playlists.
-            foreach (var searchResult in searchListResponse.Items)
-            {
-                switch (searchResult.Id.Kind)
-                {
-                    case "youtube#video":
-                        videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
-                        break;
-
-                    case "youtube#channel":
-                        channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
-                        break;
-
-                    case "youtube#playlist":
-                        playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
-                        break;
-                }
-            }
-
-            //Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
-            //Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
-            //Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
-        }
-
-        //Get Channel Videos
-        public async Task<List<YoutubeVideo>> ListItems(string searchQuery, int maxResults)
-        {
-            var searchItemsListRequest = youtubeService.Search.List("snippet");
-            searchItemsListRequest.Q = searchQuery;
-            //searchItemsListRequest.Type = "video";
-            searchItemsListRequest.Order = Google.Apis.YouTube.v3.SearchResource.ListRequest.OrderEnum.Date;
-            searchItemsListRequest.MaxResults = maxResults;
-            searchItemsListRequest.PageToken = "";
-
-            var searchItemsListResponse = await searchItemsListRequest.ExecuteAsync();
-            List<YoutubeVideo> searchItems = new List<YoutubeVideo>();
-
-            foreach (var searchItem in searchItemsListResponse.Items)
-            {
-                searchItems.Add(
-                    new YoutubeVideo
-                    {
-                        Id = searchItem.Id.VideoId,
-                        Title = searchItem.Snippet.Title,
-                        Description = searchItem.Snippet.Description,
-                        PubDate = searchItem.Snippet.PublishedAt,
-                        Thumbnail = searchItem.Snippet.Thumbnails.Medium.Url,
-                        YoutubeLink = "https://www.youtube.com/watch?v=" + searchItem.Id.VideoId
-                    });
-            }
-            return searchItems;
-        }
-
 
         //After selecting a video, navigate to the VideoPage
         private void Videos_ItemClick(object sender, ItemClickEventArgs e)
