d0671a2d5c8ff25667b721580a7dd402559dd4db
pham0uz <cpham@hotmail.de>
2017-06-26 14:57:17 +0200

    tried MyToolKit

--- HomeTube/Common/YoutubeVideo.cs
+++ HomeTube/Common/YoutubeVideo.cs
@@ -1,20 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Common
 {
     class YouTubeVideo
-        {
-            public string Id { get; set; }
-            public string Title { get; set; }
-            public DateTime? PubDate { get; set; }
-            public string YoutubeLink { get; set; }
-            public string PlayerLink { get; set; }
-            public string Thumbnail { get; set; }
-            public string Description { get; set; }
-        }
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public DateTime? PubDate { get; set; }
+        public string YoutubeLink { get; set; }
+        public string PlayerLink { get; set; }
+        public string Thumbnail { get; set; }
+        public string Description { get; set; }
     }
 }
--- HomeTube/HomeTube/MainPage.xaml.cs
+++ HomeTube/HomeTube/MainPage.xaml.cs
@@ -13,12 +13,15 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-using Google.Apis.Auth.OAuth2;
+//using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Upload;
 using Google.Apis.Util.Store;
-using Google.Apis.YouTube.v3;
-using Google.Apis.YouTube.v3.Data;
+//using Google.Apis.YouTube.v3;
+//using Google.Apis.YouTube.v3.Data;
+using MyToolkit.Multimedia;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
 
 /// <summary>
 /// YouTube Data API v3 sample: search by keyword.
@@ -40,6 +43,26 @@ namespace HomeTube
         public MainPage()
         {
             this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(1080, 1920);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        async Task<Uri> GetYoutubeUri(string VideoID)
+        {
+            YouTubeUri uri = await YouTube.GetVideoUriAsync(VideoID, YouTubeQuality.Quality1080P);
+            return uri.Uri;
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            Uri _videoUri = await GetYoutubeUri("HA_0TmL_mVU");
+            if (_videoUri != null)
+            {
+                player.Source = _videoUri;
+                player.Play();
+            }
         }
     }
+
 }
